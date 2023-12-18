6b46c4f3f3e9ab46044e2888854596d4a79640a0
Benji Loya <benjiloya.apple@gmail.com>
2023-12-18 21:14:01 +0500

    Initial Commit

---
+++ Animated Toast/Animated_ToastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_ToastApp.swift
+//  Animated Toast
+//
+//  Created by Benji Loya on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Animated_ToastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
