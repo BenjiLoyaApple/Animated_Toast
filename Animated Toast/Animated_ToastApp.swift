456992f623f89fba819f1fc1670ef23708d44272
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-11-30 10:41:40 +0500

    Complete

--- Animated Toast/Animated_ToastApp.swift
+++ Animated Toast/Animated_ToastApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Animated_ToastApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+      //      RootView {
+                ContentView()
+       //     }
         }
     }
 }
--- Animated Toast/ContentView.swift
+++ Animated Toast/ContentView.swift
@@ -10,15 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Present Toast") {
+                Toast.shared.present(
+                    title: "AirPods Pro",
+                    symbol: "airpodspro",
+                    isUserInteractionEnabled: true,
+                    timing: .long
+                )
+            }
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    RootView {
+        ContentView()
+    }
 }
